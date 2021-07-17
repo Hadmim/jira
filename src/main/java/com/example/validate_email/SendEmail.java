345e7a569d99133dc837e3c56c9ebe665149904f
Hadmim <85761198+Hadmim@users.noreply.github.com>
2021-07-17 16:31:55 +0200

    Update SendEmail.java

--- src/main/java/com/example/validate_email/SendEmail.java
+++ src/main/java/com/example/validate_email/SendEmail.java
@@ -1,35 +1,15 @@
 package com.example.validate_email;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.jsoup.Jsoup;
 import org.springframework.beans.factory.annotation.Value;
-import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Profiles;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
 import  com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
 import com.sendgrid.Method;
 import com.sendgrid.Request;
 import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 import org.springframework.core.env.Environment;
-import  com.sendgrid.helpers.mail.objects.Attachments;
-import  org.apache.commons.codec.binary.Base64;
-
-import javax.annotation.PostConstruct;
 
 @Service
 public class SendEmail {
@@ -41,21 +21,6 @@ public class SendEmail {
     public Boolean sendEmail(Mail mail) throws IOException {
         // the sender email should be the same as we used to Create a Single Sender Verification
 
-      /* Email from = new Email(EmailSers);
-        Email to = new Email(EmailDes);
-        Content cnt;
-        if (!content.equals(Jsoup.parse(content).text())) {
-            // There was HTML in the text.
-           cnt = new Content("text/html", content);
-        }
-        else{
-           cnt = new Content("text/plain", content);
-        }
-
-        Mail mail = new Mail(from, subject, to, cnt);
-        for (Attachments atch:atchs){
-            mail.addAttachments(atch);
-        }*/
        if(SendService("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",mail))
            return true;
        else
