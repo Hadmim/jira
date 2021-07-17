3282b3f843b73bab8a101450562391980b99db35
Hadmim <85761198+Hadmim@users.noreply.github.com>
2021-07-17 16:40:35 +0200

    Update ValidateEmail.java

--- src/main/java/com/example/validate_email/ValidateEmail.java
+++ src/main/java/com/example/validate_email/ValidateEmail.java
@@ -1,39 +1,18 @@
 package com.example.validate_email;
 
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Attachments;
-import com.sendgrid.helpers.mail.objects.Content;
-import org.apache.commons.codec.binary.Base64;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.context.EnvironmentAware;
-import org.springframework.core.env.Profiles;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.file.Files;
-import java.security.Key;
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-import javax.mail.Session;
 import javax.mail.*;
-import javax.mail.internet.*;
-import javax.activation.*;
+
 
 
 @Service
@@ -90,35 +69,6 @@ public JSONObject ValidService(String email,String Key) {
 }
 
 
-
-
-
-    public static void main(String[] args) throws IOException, JSONException, MessagingException {
-
-      /*  List<String> files=new ArrayList<>();
-
-        SendEmail send=new SendEmail();
-
-
-        Mail mail_ache= EmailGeneric.email("hadjer.mimoune@univ-constantine2.dz","hadjer.mimoune@univ-constantine2.dz",
-                EmailGeneric.subject2,EmailGeneric.AcheteurDomicile,
-                "1001",files,"Batna","Batna",
-                "0500","300","0673565993",
-                "lanohoza@gmail.com","https://maps.app.goo.gl/61NhouoeniRm4DGG6",
-                "","","","","","","");
-
-        files.add("C:/Users/Hadjer/Desktop/Bon livraison.pdf");
-
-        Mail mail_vend=   EmailGeneric.email("hadjer.mimoune@univ-constantine2.dz","hadjer.mimoune@univ-constantine2.dz",
-                 EmailGeneric.subject1,EmailGeneric.VendeurDahaB,"1001",
-                 files , "","","","",
-                "", "","","","","",
-                 "","","","");
-        send.sendEmail(mail_ache);
-        send.sendEmail(mail_vend);*/
-
-
-    }
 
 
 }
